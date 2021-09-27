2ff2eed49935b0ced5dafd92f5225e3f9fc8e7d8
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-09-27 16:07:34 -0400

    minor fix

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --epochs 100 --batch_size 4 --resume ./detr-r50-e632da11.pth --coco_path ../coco_10 --output_dir ./output_0
+python main.py --epochs 100 --batch_size 6 --resume ./detr-r50-e632da11.pth --coco_path ../coco_10 --output_dir ./output_0
