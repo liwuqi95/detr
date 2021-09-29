1bc2fa5a1008b33e7e767bf9eba01361bd0a9484
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-09-29 15:54:30 -0400

    minor fix

--- train-2.sh
+++ train-2.sh
@@ -1,2 +1,2 @@
-FOLDER_COUNT=ls -l ./outputs | grep -c ^d
+FOLDER_COUNT=$(ls -l ./outputs | grep -c ^d)
 python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --epochs 150 --batch_size 4 --resume ./detr-r50-e632da11.pth --coco_path ../coco_10 --output_dir ./outputs/$FOLDER_COUNT
